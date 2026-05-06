ca3a3a888d2b0bc233425acb199620937b4db42f
Rajvir Singh <rajvir@apyhub.com>
2026-05-06 16:03:21 +0530

    chore: updated Dockerfile and build.sh path

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -1,10 +1,10 @@
-#build and push docker images to AWS
+#!/bin/sh
+
 echo "Login to ECR Repository"
 echo "Preparation task"
+echo "Build Docker Image"
 aws configure
+docker build --build-arg DEPLOYMENT_ID=${CI_PIPELINE_ID} -t $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID -f Dockerfile .
+echo "Push to ECR Repository"
 aws ecr get-login-password | docker login --username AWS --password-stdin $DOCKER_REGISTRY
-
-echo "Build App Docker Image"
-docker build --build-arg DEPLOYMENT_ID=${CI_PIPELINE_ID} -t $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID -f e2e/dashboard/Dockerfile e2e/dashboard
-echo "Push App Image to ECR"
 docker push $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID
