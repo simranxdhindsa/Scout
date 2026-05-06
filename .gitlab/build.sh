4cbc65cd9d90b48387a3cc449ff54c6d2e6eae4c
Manveer Singh <manveersingh@Manveers-MacBook-Air.local>
2026-05-06 14:33:42 +0530

    creating build

---
+++ .gitlab/build.sh
@@ -0,0 +1,10 @@
+#build and push docker images to AWS
+echo "Login to ECR Repository"
+echo "Preparation task"
+aws configure
+aws ecr get-login-password | docker login --username AWS --password-stdin $DOCKER_REGISTRY
+
+echo "Build App Docker Image"
+docker build --build-arg DEPLOYMENT_ID=${CI_PIPELINE_ID} -t $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID -f test-cases/e2e/dashboard/Dockerfile test-cases/e2e/dashboard
+echo "Push App Image to ECR"
+docker push $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID
