d0ca10370476ffbfec01e4cf7ab940ae8adf16f2
Rajvir Singh <rajvir@apyhub.com>
2026-05-06 16:34:46 +0530

    FIX: added aws ECR login creds

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -5,6 +5,6 @@ echo "Preparation task"
 echo "Build Docker Image"
 aws configure
 docker build --build-arg DEPLOYMENT_ID=${CI_PIPELINE_ID} -t $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID -f Dockerfile e2e/dashboard
-
 echo "Push to ECR Repository"
+aws ecr get-login-password | docker login --username AWS --password-stdin $DOCKER_REGISTRY
 docker push $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID
