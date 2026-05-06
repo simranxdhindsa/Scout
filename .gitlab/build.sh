1a8c1c7a471bcad48a15d05351ad79f5d1f39c99
Rajvir Singh <rajvir@apyhub.com>
2026-05-06 16:16:28 +0530

    chore: updated the -f flag, and conf files

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -4,7 +4,7 @@ echo "Login to ECR Repository"
 echo "Preparation task"
 echo "Build Docker Image"
 aws configure
-docker build --build-arg DEPLOYMENT_ID=${CI_PIPELINE_ID} -t $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID -f Dockerfile .
+docker build --build-arg DEPLOYMENT_ID=${CI_PIPELINE_ID} -t $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID -f Dockerfile e2e/dashboard
+
 echo "Push to ECR Repository"
-aws ecr get-login-password | docker login --username AWS --password-stdin $DOCKER_REGISTRY
 docker push $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID
