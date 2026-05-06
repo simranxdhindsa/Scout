ff857f0122dc788473965f973ab797d8f797d4c2
Rajvir Singh <rajvir@apyhub.com>
2026-05-06 15:33:01 +0530

    changed path in build.sh

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -5,6 +5,6 @@ aws configure
 aws ecr get-login-password | docker login --username AWS --password-stdin $DOCKER_REGISTRY
 
 echo "Build App Docker Image"
-docker build --build-arg DEPLOYMENT_ID=${CI_PIPELINE_ID} -t $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID -f test-cases/e2e/dashboard/Dockerfile test-cases/e2e/dashboard
+docker build --build-arg DEPLOYMENT_ID=${CI_PIPELINE_ID} -t $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID -f e2e/dashboard/Dockerfile e2e/dashboard
 echo "Push App Image to ECR"
 docker push $DOCKER_REGISTRY/$REPO:$PREFIX-$CI_PIPELINE_ID
